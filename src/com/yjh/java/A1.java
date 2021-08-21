20d9a8ec9570af834f3a0a0d8663e349c1faed11
YJH <ash_yjh@163.com>
2021-08-21 16:51:49 +0800

    Initial commit

---
+++ src/com/yjh/java/A1.java
@@ -0,0 +1,8 @@
+package com.yjh.java;
+
+/**
+ * @author lenovo
+ * @create 2021-08-20 16:13我
+ */
+public class A1 {
+}
---
+++ src/com/yjh/java/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.yjh.java;
+
+
+import java.util.Map;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+    }
+
+
+}
